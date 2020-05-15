955be94080328621d27a8a87be6a70d5ef1be777
Aly Suleiman <alymosul@gmail.com>
2020-05-15 17:17:51 +0200

    throw Exception if debug is enabled..

--- lib/Expo.php
+++ lib/Expo.php
@@ -108,7 +108,7 @@ class Expo
         $response = $this->executeCurl($ch);
 
         // If the notification failed completely, throw an exception with the details
-        if (!$debug && $this->failedCompletely($response, $recipients)) {
+        if ($debug && $this->failedCompletely($response, $recipients)) {
             throw ExpoException::failedCompletelyException($response);
         }
 
